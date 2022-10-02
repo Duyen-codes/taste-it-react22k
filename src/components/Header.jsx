5a4b3783ab6c155bdc004067b64c3330a3e37483
Duyen Nguyen <hongduyen0705@gmail.com>
2022-10-02 11:07:22 +0300

    added padding for mobile menu item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = (props) => {
           <div className={styles["mobile-navigation__menu"]}>
             <ul className={styles["mobile-navigation__menu-items"]}>
               <li>
-                <Link to="/Q&A">Q&A</Link>
+                <Link to="/qa">Q&A</Link>
               </li>
               <li>
                 <Link to="contact">Contact</Link>
