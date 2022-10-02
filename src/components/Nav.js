158f795b7542f8e50bd9cfa4473b5f92502311e7
Duyen Nguyen <hongduyen0705@gmail.com>
2022-10-02 15:19:50 +0300

    styled user-info-item nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,21 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { IconButton } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 import styles from "./Nav.module.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const Nav = (props) => {
   const { savedRecipes } = props;
   let activeStyle = {
     textDecoration: "underline",
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleNavItemClick = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
@@ -59,8 +66,13 @@ const Nav = (props) => {
             {savedRecipes.length > 0 ? savedRecipes.length : ""}
           </NavLink>
         </li>
-        <li className={styles["nav-item"]}>
-          <NavLink to="/login">
+        <li
+          className={`${styles["nav-item"]} ${styles["user-info-item"]} ${
+            isOpen ? `${styles["nav-item--open"]}` : ""
+          }`}
+          onClick={handleNavItemClick}
+        >
+          <div className={styles["main-navi-user-info-icon-container"]}>
             <IconButton
               sx={{
                 color: "#c2ae94",
@@ -68,7 +80,40 @@ const Nav = (props) => {
             >
               <PersonIcon fontSize="large" />
             </IconButton>{" "}
-          </NavLink>
+          </div>
+          <span className={styles["main-navi-item-description"]}>
+            <span className={styles["login"]}>Login</span>
+            <KeyboardArrowDownIcon
+              className={styles["down-arrow"]}
+              fontSize="large"
+            />
+          </span>
+
+          <div className={styles["menu-mobile-overlay"]}>
+            {isOpen && (
+              <nav id={styles["profile-menu"]}>
+                <ul>
+                  <li className={styles["menu-item"]}>
+                    <div className={styles["login-controls"]}>
+                      <NavLink
+                        to="/login"
+                        type="button"
+                        className={`${styles["btn"]} ${styles["btn--primary"]} ${styles["login-button"]} `}
+                      >
+                        Login
+                      </NavLink>
+                      <NavLink
+                        to="/register"
+                        className={`${styles["btn"]} ${styles["btn--borderless"]} ${styles["register-button"]} `}
+                      >
+                        Register
+                      </NavLink>
+                    </div>
+                  </li>
+                </ul>
+              </nav>
+            )}
+          </div>
         </li>
       </ul>
     </nav>
